f713ba2bad33e82efa83f4f89b29eec7069a1ed2
Glitch (floxy-0) <none>
2022-03-10 22:26:35 +0000

    🐁😻 Updated with Glitch

---
+++ events/ready.js
@@ -0,0 +1,22 @@
+const chalk = require('chalk');
+const moment = require('moment');
+const Discord = require('discord.js');
+const ayarlar = require('../ayarlar.json');
+var prefix = ayarlar.prefix;
+
+module.exports = client => {
+ setInterval(function() {
+}, 8000);
+  var msgArray = [
+"Floxy🔥",
+"!yardım, !ekonomisistemi, !moderasyon",
+"",
+    ""
+ ];
+
+ setInterval(() => {
+  var rastgeleOyun = Math.floor(Math.random() * msgArray.length);
+  client.user.setActivity(`${msgArray[rastgeleOyun]}`, { type: 'STREAMING' ,  url: 'https://www.twitch.tv/mbfloxy' })
+}, 5000);
+    console.log(`Bot Hazır | youtube.com/floxy`);
+}
